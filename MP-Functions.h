64755037d2ee411b4d86991113dc1ba40f5675ed
Z4ri <161800566+Z4ri@users.noreply.github.com>
2025-03-16 00:44:04 +0800

    Filled in with headers

---
+++ MP-Functions.h
@@ -0,0 +1,10 @@
+#ifndef MP_FUNCTIONS_H
+#define MP_FUNCTIONS_H
+
+#include <stdio.h>
+
+// Function declarations
+void exampleFunction1();
+int exampleFunction2(int param);
+
+#endif // MP_FUNCTIONS_H
--- MP-Main.c
+++ MP-Main.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include "MP-Functions.h"
+#include "MP-Functions.c"
+
+int main()
+{
+    
+    return 0;
+}
